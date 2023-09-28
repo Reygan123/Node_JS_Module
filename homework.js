dce8af229f112d80c97a17f68af6b1aca52ce2bd
unknown <fadhilahreygan@gmail.com>
2023-09-28 08:46:14 +0700

    Homework Week 7

---
+++ homework.js
@@ -0,0 +1,15 @@
+exports.luasPersegi = (sisi1, sisi2) => {
+  return sisi1 * sisi2;
+};
+
+exports.kelilingPersegi = (sisi) => {
+  return 4 * sisi;
+};
+
+exports.luasPersegiPanjang = (panjang, lebar) => {
+  return panjang * lebar;
+};
+
+exports.kelilingPersegiPanjang = (panjang, lebar) => {
+  return 2 * panjang + 2 * lebar;
+};
---
+++ soal1.js
@@ -0,0 +1,10 @@
+//memanggil module yang ada di file homework.js
+const luas = require("./homework.js");
+
+console.log(luas.luasPersegi(4, 5));
+
+console.log(luas.kelilingPersegi(4));
+
+console.log(luas.luasPersegiPanjang(4, 5));
+
+console.log(luas.kelilingPersegiPanjang(4, 5));
